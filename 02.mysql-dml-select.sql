a1a3f536d16fd6e3844f4a5ee199b8792d0f77e2
kimyk0101 <kimyk7131@gmail.com>
2024-11-18 18:26:21 +0900

    Update 02.mysql-dml-select.sql

--- 02.mysql-dml-select.sql
+++ 02.mysql-dml-select.sql
@@ -235,3 +235,45 @@ SELECT CAST("123" AS UNSIGNED);	-- 문자열 -> UNSIGNED INT
 -- CONVERT 함수로 대신할 수 있음
 SELECT CONVERT(now(), DATE) FROM DUAL;
 SELECT CONVERT("123", UNSIGNED) FROM DUAL;
+
+
+----------------------------
+-- 연습문제
+----------------------------
+
+-- 문제1.
+SELECT CONCAT(first_name, ' ', last_name) AS 이름, salary AS 월급, phone_number AS 전화번호, hire_date AS 입사일 
+	FROM employees ORDER BY hire_date, first_name;
+    
+-- 문제2.
+SELECT job_title, max_salary, salary FROM employees, jobs ORDER BY salary DESC;
+
+-- 문제3.
+SELECT CONCAT(first_name, ' ', last_name) AS 이름, manager_id, commission_pct, salary FROM employees 
+	WHERE manager_id IS NOT NULL AND commission_pct IS NULL AND salary > 3000
+	ORDER BY salary DESC;
+    
+-- 문제4.
+SELECT job_title, max_salary FROM jobs WHERE max_salary >=10000 ORDER BY max_salary DESC;
+
+-- 문제5.
+SELECT first_name, salary, IFNULL(commission_pct, 0) FROM employees WHERE salary < 14000 AND salary >= 10000  ORDER BY salary DESC;
+
+-- 문제6.
+SELECT CONCAT(first_name, ' ', last_name) AS 이름, DATE_FORMAT(hire_date, '%Y-%m') AS 입사일, department_id AS 부서번호
+	FROM employees WHERE department_id IN(10, 90, 100);
+    
+-- 문제7.
+SELECT CONCAT(first_name, ' ', last_name) AS 이름, salary FROM employees 
+	WHERE LOWER(first_name) LIKE '%s%' OR UPPER(first_name) LIKE '%S%';
+    
+-- 문제8.
+SELECT department_name FROM departments ORDER BY LENGTH(department_name) DESC;
+
+-- 문제9.
+SELECT UPPER(country_name) FROM countries ORDER BY country_name;
+
+-- 문제10.
+SELECT CONCAT(first_name, ' ', last_name) AS 이름, salary, REPLACE(phone_number, ".", "-")As phone_number, hire_date FROM employees
+WHERE hire_date < '2003-12-31';
+-- 545.343.3433 -> 545-343-3433
